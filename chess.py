c3d70dd1e3c8a15d2d71d8ab2f6095ffaf93d5d1
Mark <kresmark983@gmail.com>
2025-02-01 11:48:43 +0700

    1-3,5

--- chess.py
+++ chess.py
@@ -5,6 +5,10 @@
 # Визуализация: Используйте тепловую карту (imshow), чтобы показать шахматную доску. Отметьте положение ферзя и атакуемые клетки цветами.
 import matplotlib.pyplot as plt
 
+def ATTACK(x,y):
+    if 0<=x<8 and 0<=y<8:
+        ax.add_patch(plt.Circle((x,y),0.2,color="orange", alpha=1))
+
 chess=[[0 for _ in range(8)] for _ in range(8)]
 for i in range(8):
     for j in range(8):
@@ -27,13 +31,22 @@ ax.set_xticks(list(range(8)), labels=xlabels)
 ylabels = [f'{y}' for y in range(1,9)]
 ax.set_yticks(list(range(8)), labels=ylabels)
 
+#for i in range(8):
+#    if i!=x:
+#        circle=plt.Circle((i,y),0.2,color="orange",alpha=1)
+#        ax.add_patch(circle)
+#    if i!=y:
+#        circle=plt.Circle((x,i),0.2,color="orange",alpha=1)
+#        ax.add_patch(circle)
 for i in range(8):
     if i!=x:
-        circle=plt.Circle((i,y),0.2,color="orange",alpha=1)
-        ax.add_patch(circle)
+        ATTACK(i,y)
     if i!=y:
-        circle=plt.Circle((x,i),0.2,color="orange",alpha=1)
-        ax.add_patch(circle)
-
+        ATTACK(x,i)
+for i in range(1,8):
+    ATTACK(x+i,y+i)
+    ATTACK(x-i,y+i)
+    ATTACK(x+i,y-i)
+    ATTACK(x-i,y-i)
 plt.show()
 
--- wether.py
+++ wether.py
@@ -8,3 +8,23 @@
 # Линейный график температуры по дням.
 # Гистограмму распределения температуры.
 # Подсветку "холодных" и "жарких" дней на линейном графике.
+import numpy as np
+import matplotlib.pyplot as plt
+mas=np.random.randint(-10,35,size=365)
+print(mas.mean())
+maximum=0
+cur=0
+for i in mas:
+    if i>0:
+        cur+=1
+    else:
+        maximum=max(cur,maximum)
+        cur=0
+print(maximum)
+unique,counts=np.unique(mas,return_counts=True)
+plt.subplot(1,2,1)
+plt.bar(unique,counts)
+x=[i for i in range(1,366)]
+plt.subplot(1,2,2)
+plt.plot(x,mas)
+plt.show()
